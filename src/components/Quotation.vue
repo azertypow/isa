8a6ac1367129a58eadd5d714d6458e966f6d1b64
azertypow <azertypow@gmail.com>
2018-11-14 00:05:38 +0100

    feat: text in quotation page

--- src/components/Quotation.vue
+++ src/components/Quotation.vue
@@ -2,7 +2,13 @@
     <section class="v-quotation">
         <button class="previous-button" v-on:click="$emit('previousPage')" >return</button>
 
-        quotation
+        <div class="wrap">
+            <div class="content">
+
+                <p>Vous pouvez à tout moment prendre contact avec moi :</p>
+
+            </div>
+        </div>
 
         <button class="next-button"     v-on:click="$emit('nextPage')"     >continue</button>
     </section>
@@ -19,8 +25,20 @@
 
 <style lang="scss">
     @import "../scss/pages";
+    @import "../scss/_grid";
 
     .v-quotation {
-        @include page-element("white")
+        @include page-element("white");
+
+        .wrap {
+            @include column-container;
+            @include container-content-centred;
+            @include site-max-width;
+            height: 100%;
+        }
+
+        .content {
+            @include column(8, 12);
+        }
     }
 </style>
